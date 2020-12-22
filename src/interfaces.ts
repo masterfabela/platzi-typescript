c8678198d80f7eebc9d5c5bb95bcdc5656fa3473
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-22 03:44:52 +0100

    Clase 22

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -36,3 +36,41 @@ showPicture({
 	orientation: PhotoOrientation.Portrair,
 	// extra: 'test',
 });
+
+interface PictureConfig {
+	title?: string;
+	date?: string;
+	orientation?: PhotoOrientation;
+}
+
+function generatePicture(config: PictureConfig) {
+	const picture = { title: 'default', date: '2020-03' };
+	if (config.title) {
+		picture.title = config.title;
+	}
+	if (config.date) {
+		picture.date = config.date;
+	}
+	return picture;
+}
+
+let picture = generatePicture({});
+console.log('picture: ', picture);
+picture = generatePicture({ title: 'Travel Pic' });
+console.log('picture: ', picture);
+picture = generatePicture({ title: 'Travel Pic', date: '2021-05' });
+console.log('picture: ', picture);
+
+// Interfaz: Usuario
+interface User {
+	readonly id: number;
+	username: string;
+	isPro: boolean;
+}
+
+let user: User;
+user = { id: 10, username: 'joserra', isPro: true };
+console.log('User: ', user);
+user.username = 'Luis';
+// user.id = 5; // error
+console.log('User: ', user);
